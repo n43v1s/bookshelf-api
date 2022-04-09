c0825c01c162c3cdf0b9f5a7122b3e7d3589cf03
n43v1s <agusrokyanto@gmail.com>
2022-04-09 16:28:22 +0700

    adding Handler and Routes

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+const { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler, deleteBookById } = require("./handler");
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/books',
+        handler: addBookHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getAllBooksHandler,
+    },
+    {
+        method: 'GET',
+        path: '/books/{id}',
+        handler: getBookByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/books/{id}',
+        handler: editBookByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{id}',
+        handler: deleteBookById,
+    },
+];
+
+module.exports = routes;
